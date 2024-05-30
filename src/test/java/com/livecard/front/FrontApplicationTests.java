685b16ef740fabe6c681be2dc9cfd8d4b1826efc
lee seonguk <passion1014@gmail.com>
2024-05-30 19:40:32 +0900

    초기화 작업

---
+++ src/test/java/com/livecard/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livecard.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
